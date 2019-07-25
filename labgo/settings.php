42959f2e614ac185facc653af695a721869ba1ff
CrazimanCode <53278312+CrazimanCode@users.noreply.github.com>
2019-07-25 09:43:15 -0300

    Create settings.php

---
+++ labgo/settings.php
@@ -0,0 +1,12 @@
+<?php
+
+/* Google App Client Id */
+define('CLIENT_ID', '676934714863-tongo2ij6oqfgdom1j1mgknqu6h7er07.apps.googleusercontent.com');
+
+/* Google App Client Secret */
+define('CLIENT_SECRET', '1qHXorfYd_7eMUQVGuJAlxHJ');
+
+/* Google App Redirect Url */
+define('CLIENT_REDIRECT_URL', 'https://www.websiteseguro.com/labgo');
+
+?>
